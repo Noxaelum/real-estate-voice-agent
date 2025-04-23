94fc3f1869b11b30cb01d65d542038613e4dbd92
Sly Villafania <villafania.sly.lau@gmail.com>
2025-04-23 22:33:20 +0800

    Add elevenlabs and nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  const agentId = process.env.NEXT_PUBLIC_ELEVENLABS_AGENT_ID ?? "";
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <section className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold">Real Estate voice agent</h1>
+        <p>Start by pressing the &quot;start a call&quot; below</p>
+      </section>
+      <elevenlabs-convai agent-id={agentId}></elevenlabs-convai>
+      <script
+        src="https://elevenlabs.io/convai-widget/index.js"
+        async
+        type="text/javascript"
+      ></script>
+    </main>
+  );
+}
